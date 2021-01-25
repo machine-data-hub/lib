df1a41caa5e95cbd96b4fd25e563b64ba83dda4c
ceciliabarnes <cbarnes7@uw.edu>
2021-01-25 15:42:35 -0800

    adding basic download and get info functionality for 1 dataset

--- src/phm_data_hub/cli.py
+++ src/phm_data_hub/cli.py
@@ -2,16 +2,41 @@ import typer
 
 app = typer.Typer()
 
-@app.command()
-def hello(name: str):
-    typer.echo(f"Hello {name}")
+datasets = {"Combined Cycle Power Plant Data Set": {"Link":"https://archive.ics.uci.edu/ml/machine-learning-databases/00294/CCPP.zip",
+                                                "Data Set Characteristics":"Multivariate",
+                                                "Associated Tasks":"Regression",
+                                                "Number of Instances": 9568,
+                                                "Number of Attributes": 4,
+                                                "Missing Values?":"N/A",
+                                                "Date Donated":"2014-03-26",
+                                                "Number of Web Hits":191037}}
+suggestions = []
+@app.command("suggest")
+def hello(link: str):
+    suggestion = link
+    typer.echo(f"Thank you! You have suggested a dataset from the following link: {link}")
 
-@app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        typer.echo(f"Goodbye Ms. {name}. Have a good day.")
-    else:
-        typer.echo(f"Bye {name}!")
+@app.command("dataset")
+def dataset(action: str, name: str):
+    """You can directly download the full dataset by passing "download" for the action argument.
+        If you'd rather view the dataset's metadata, pass "get info" for the action argument.
+    """
+    if action == "download":
+        if name in datasets.keys():
+            typer.echo(f"Downloading {name} as zip file")
+            typer.launch(datasets.get(name).get("Link"))
+            #typer.launch("https://archive.ics.uci.edu/ml/machine-learning-databases/00294/CCPP.zip")
+    elif action == "get info":
+        info = ""
+        for key in datasets.get(name).keys():
+            info += key
+            info += ": "
+            info += str(datasets.get(name).get(key))
+            info += "\n"
+        typer.echo(info)
+        #typer.echo("Taking you to view parent directory")
+        #typer.launch("https://archive.ics.uci.edu/ml/machine-learning-databases/")
 
-def main():
+
+if __name__ == "__main__":
     app()
